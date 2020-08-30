05792183852140524305b8c62d0bc0c0ff9414f0
yo-cleon <cleon.form@gmail.com>
2020-08-30 11:59:36 +0200

    Inicialización del repositorio

---
+++ README.md
@@ -0,0 +1,11 @@
+# Despensa
+
+Aplicación desarrollada para gestionar la despensa y las compras de un hogar.
+
+## Requerimientos
++ Node 12.18.3
++ npm 6.14.6
++ Angular 10.0.14
++ Angular CLI 10.0.8
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserLoginComponent } from './users/user-login/user-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
