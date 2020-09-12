eb2abdae7cb68d4c0bd5c14ed02bf033ca06b0ff
yo-cleon <cleon.form@gmail.com>
2020-09-12 18:52:54 +0100

    Añadida funcionalidad para modificar el perfil de usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
 import { map } from 'rxjs/operators';
+import { FirebaseApp } from '@angular/fire';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor( private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth,
+              private firebase: FirebaseApp) { }
 
   loginUser(email: string, pass: string) {
     return new Promise ((resolve, reject) => {
@@ -32,6 +33,31 @@ export class AuthService {
           .then(userData => resolve(userData), err => reject(err));
       }
     )
+  }
+
+  updateUser(usName: string, photo: string): boolean {
+    var result: boolean;
+    var user = this.firebase.auth().currentUser;
+    user.updateProfile({
+      displayName: usName,
+      photoURL: photo
+    }).then(res => {
+        alert("Perfil Actualizado");
+        result = true;
+      })
+    .catch(err => {
+        console.log("Ha habido un error actualizando el perfil");
+        result = false;
+      })
+    return result;
+  }
+
+  updateUserEmail(email: string){
+    var user = this.firebase.auth().currentUser;
+    console.log("Email a actualizar: "+email);
+    user.updateEmail(email)
+      .then(res => console.log("Email actualizado"))
+      .catch(err => console.log("Error actualizando el email"))
   }
 
 }
--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseApp } from '@angular/fire';
-import { Observable } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,12 +10,34 @@ import { Observable } from 'rxjs';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor(private firebase: FirebaseApp) { }
+  email: string;
+  userName: string;
+  photo: string;
+
+  constructor(private firebase: FirebaseApp,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
-    //this.user = this.firebase.auth().currentUser;
-    console.log(this.firebase.auth().currentUser);
+    this.getUserData();
   }
 
-  user: Observable<firebase.User | null>
+  onUpdateUser(profileForm: NgForm) {
+    if (this.authService.updateUser(profileForm.value.userName, profileForm.value.photo)){
+      alert("Perfil actualizado correctamente");
+      this.ngOnInit();
+    } else {
+      alert("Ha habido problemas actualizando el perfil");
+    }
+    this.authService.updateUserEmail(profileForm.value.email);
+  }
+
+  getUserData() {
+    var user = this.firebase.auth().currentUser;
+    if (user) {
+      this.userName = user.displayName;
+      this.email = user.email;
+      this.photo = user.photoURL;
+    }
+
+  }
 }
